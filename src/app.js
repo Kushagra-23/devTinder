45ee1b78c35d9488210afa1a6f8685d4867b56ad
Kushagra-23 <kshgrkumar@gmail.com>
2024-10-21 21:02:41 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res) => {
-  res.send("Namaste from server")
-}) // this function is known as request handler
-
-app.use('/hello', (req, res) => {
-  res.send("Hello from server")
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Kushagra", lastName: 'Kumar' })
 })
 
+// This will only handle POST call to /user
+app.post("/user", (req, res) => {
+  console.log("Saved data to database")
+  res.send('Data saved successfully')
+})
+
+// This will only handle DELETE call to /user
+app.delete("/user", (req, res) => {
+  console.log("Deleted data to database")
+  res.send('Data deleted successfully')
+})
+
+// This will match all the HTTP method API calls to /test
 app.use('/test', (req, res) => {
   res.send("Hello test from server")
 })
