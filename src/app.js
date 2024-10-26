e4c409ce762c851e302caa18d3e43175db758c7d
Kushagra-23 <kshgrkumar@gmail.com>
2024-10-26 20:35:17 +0530

    mutliple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,29 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.get("/ab?c", (req, res) => {
-  res.send({ firstName: "Kushagra", lastName: 'Kumar' })
-}) // It will work for /ac, /abc
+// app.use('/route', rH1, [rH2, rH3], rH4, rH5);
 
-app.get("/ab+c", (req, res) => {
-  res.send({ firstName: "Kushagra", lastName: 'Kumar' })
-}) // It will work for /abc, /abbc, /abbbbc 
-
-app.get("/ab*cd", (req, res) => {
-  res.send({ firstName: "Kushagra", lastName: 'Kumar' })
-}) // It will work for /abKUSHAGRAcd, /ab1234c
-
-app.get("/a(bc)?d", (req, res) => {
-  res.send({ firstName: "Kushagra", lastName: 'Kumar' })
-}) // It will work for /ad and /abcd
-
-app.get(/a/, (req, res) => {
-  res.send({ firstName: "Kushagra", lastName: 'Kumar' })
-}) // Regex expressions
-
-app.get("/user/:id", (req, res) => {
-  res.send({ firstName: "Kushagra", lastName: 'Kumar' })
-}) // Dynamic routes
+app.use("/user", (req, res, next) => {
+  console.log("first")
+  next();
+},
+(req, res, next) => {
+  console.log("second")
+  next();
+},
+(req, res, next) => {
+  console.log("third")
+  next();
+},
+(req, res, next) => {
+  console.log("fourth")
+  next();
+},
+(req, res, next) => {
+  console.log("fifth")
+  res.send("5") 
+})
 
 app.listen(3000, () => {
   console.log("SERVER RUNNING")
