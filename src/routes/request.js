06c9ba85613d991d698fdfdcab2ed2879d0d8b7c
Kushagra-23 <kshgrkumar@gmail.com>
2025-01-22 21:00:08 +0530

    queries and indexs in DB and handled corner cases

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,14 +1,68 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const requireRouter = express.Router();
 
-requireRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  try {
-    res.send(req.user.firstName + " Connection Request Sent");
-  } catch (error) {
-    res.status(400).send("ERROR : " + error.message);
+requireRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const allowedStatus = ["ignored", "interested"];
+
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({
+          message: "Invalid status type: " + status,
+        });
+      }
+
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(400).json({
+          message: "User not found",
+        });
+      }
+
+      // If there is an existing ConnectionRequest
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          {
+            fromUserId,
+            toUserId,
+          },
+          {
+            fromUserId: toUserId,
+            toUserId: fromUserId,
+          },
+        ],
+      });
+
+      if (existingConnectionRequest) {
+        return res.status(400).json({
+          message: "Connection Request Already Exists",
+        });
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      const data = await connectionRequest.save();
+      res.json({
+        message: req.user.firstName + " is " + status + "in " + toUser.firstName,
+        data,
+      });
+    } catch (error) {
+      res.status(400).send("ERROR : " + error.message);
+    }
   }
-});
+);
 
 module.exports = requireRouter;
