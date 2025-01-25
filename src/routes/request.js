22dd50c43024fca6c27dfbca330e3fe3c9a50876
Kushagra-23 <kshgrkumar@gmail.com>
2025-01-25 23:15:03 +0530

    user requests and connections api done

--- src/routes/request.js
+++ src/routes/request.js
@@ -56,7 +56,50 @@ requireRouter.post(
       });
       const data = await connectionRequest.save();
       res.json({
-        message: req.user.firstName + " is " + status + "in " + toUser.firstName,
+        message:
+          req.user.firstName + " is " + status + "in " + toUser.firstName,
+        data,
+      });
+    } catch (error) {
+      res.status(400).send("ERROR : " + error.message);
+    }
+  }
+);
+
+requireRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId } = req.params;
+
+      const allowedStatus = ["accepted", "rejected"];
+
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({
+          message: "Invalid status type: " + status,
+        });
+      }
+
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+
+      if (!connectionRequest) {
+        return res.status(400).json({
+          message: "Connection request not found",
+        });
+      }
+
+      connectionRequest.status = status;
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: "Connection request " + status,
         data,
       });
     } catch (error) {
