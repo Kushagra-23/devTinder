a4806f6a91315f6c7743ca08c9adf0096aa24305
Kushagra-23 <kshgrkumar@gmail.com>
2025-02-12 21:01:42 +0530

    cors package installed

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
