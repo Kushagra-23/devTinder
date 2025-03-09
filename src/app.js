8d9754f001d1cd23e1b4f5a3abe4af64de83fd59
Kushagra-23 <kshgrkumar@gmail.com>
2025-03-09 21:06:22 +0530

    chat feature

--- src/app.js
+++ src/app.js
@@ -4,14 +4,17 @@ const cors = require("cors");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
+const http = require("http");
 
 // Uncomment for scheduling cron job
 // require("./utils/cronjob")
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -20,17 +23,24 @@ const profileRouter = require("./routes/profile");
 const requireRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 const paymentRouter = require("./routes/payment");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requireRouter);
 app.use("/", userRouter);
 app.use("/", paymentRouter);
+app.use("/", chatRouter);
+
+const server = http.createServer(app);
+
+initializeSocket(server);
 
 connectDB()
   .then(() => {
     console.log("Database connection success");
-    app.listen(7777, () => {
+    server.listen(7777, () => {
       console.log("SERVER RUNNING");
     });
   })
